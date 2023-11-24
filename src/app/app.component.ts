900d26ae5c5d72924ab0c63d8e287d5221c2759a
ramijawadi <ramijawadi104.com>
2023-11-24 08:54:14 +0100

    add input ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,7 +16,7 @@ export class AppComponent {
   btn: string = 'click me';
   name: string = 'rami jawadi';
   desc: string = 'front end developper';
-  counter :number = 0 ;
+  counter: number = 0;
 
   //attribute
 
@@ -26,19 +28,16 @@ export class AppComponent {
   bgColor: string = 'lightBlue';
   bgDiv: string = '#22c1c3';
   parag: string = 'font-size: 50px ; color: darkblue';
-  titleColor:string='blue';
+  titleColor: string = 'blue';
+  Color: boolean = false;
 
-  decremenetCounter () {
-
-    this.counter -- ;
+  decremenetCounter() {
+    this.counter--;
   }
-  
 
-  
-  incerementCounter () {
+  incerementCounter() {
+    this.counter++;
+  }
+input: string = "initial value"
 
-  this.counter ++ ;
 }
-}
-
-
